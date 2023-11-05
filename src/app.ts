87ec04cd1a1e5e95b0fb4900ca3b31a044aedbfb
Piotr Skrzynski <pio.skrzynski@gmail.com>
2023-11-05 15:48:54 +0100

    install prisma, nodemailer, helmet

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import express, { Express, Request, Response } from "express";
+import helmet from "helmet";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.DEV_PORT || 3000;
+
+app.use(helmet());
+app.use(express.json());
+
+app.listen(port, () => {
+  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+});
